e0717ae9cf399afba80be44c6209c2f39233c1eb
DevMeley <ayantolaAbisola@outlook.com>
2024-09-08 11:39:11 +0100

    error handling added

--- script.js
+++ script.js
@@ -6,11 +6,53 @@ const calculate = document.getElementById("calculate")
 calculate.addEventListener('click', event => {
     event.preventDefault()
 
-    const getWeight = Number(myWeight.value) 
+    if (myWeight.value ==="" && myHeight.value === '') {
+        showError( myHeight, "Weight and Height cannot be empty")
+    }
+    else if (isNaN(myWeight.value) || isNaN(myHeight.value)) {
+        showError(myHeight, "Please enter valid numbers for Weight and Height")
+    } 
+    else if (Number(myHeight.value) === 0) {
+        showError(myHeight, "Height cannot be zero")
+    } 
+    else{
 
-    const getheight = Number(myHeight.value)
+        clearError(myHeight, myWeight)
 
-    const BMI = getWeight / (getheight * getheight)
+        const getWeight = Number(myWeight.value) 
 
-    displayResult.textContent = BMI
+        const getheight = Number(myHeight.value)
+
+        const BMI = getWeight / (getheight * getheight)
+
+        displayResult.textContent = BMI.toFixed(2)
+    }
+
+    // const getWeight = Number(myWeight.value) 
+
+    // const getheight = Number(myHeight.value)
+
+    // const BMI = getWeight / (getheight * getheight)
+
+    // displayResult.textContent = BMI
 })
+
+
+
+showError = (element, message) =>{
+    const inputControl = element.parentElement
+    const displayError = inputControl.querySelector(".error")
+
+    displayError.innerText = message
+    displayError.classList.add("error")
+    displayError.classList.remove('success')
+}
+
+clearError = (element) =>{
+    const inputControl = element.parentElement
+    const displayError = inputControl.querySelector(".error")
+
+    displayError.innerText = ''
+    displayError.classList.add("success")
+    displayError.classList.remove('error')
+}
